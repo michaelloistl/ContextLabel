89856233cc62768440ad7e4b7f72c3aa7804cde3
Michael Loistl <michael@aplo.co>
2015-10-13 18:18:38 +0100

    Changing to swift framework and adding podspec

--- ContextLabelTests/ContextLabelTests.swift
+++ ContextLabelTests/ContextLabelTests.swift
@@ -2,13 +2,12 @@
 //  ContextLabelTests.swift
 //  ContextLabelTests
 //
-//  Created by Michael Loistl on 25/08/2014.
-//  Copyright (c) 2014 Michael Loistl. All rights reserved.
+//  Created by Michael Loistl on 13/10/2015.
+//  Copyright © 2015 Aplo. All rights reserved.
 //
 
-import UIKit
 import XCTest
-import ContextLabel
+@testable import ContextLabel
 
 class ContextLabelTests: XCTestCase {
     
@@ -24,72 +23,14 @@ class ContextLabelTests: XCTestCase {
     
     func testExample() {
         // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock() {
+        self.measureBlock {
             // Put the code you want to measure the time of here.
         }
     }
-    
-    func testAttributesFromProperties() {
-        
-        let contextLabel = ContextLabel()
-        contextLabel.shadowColor = UIColor.redColor()
-        contextLabel.shadowOffset = CGSizeMake(1, 2)
-        contextLabel.textColor = UIColor.blueColor()
-        contextLabel.textAlignment = .Center
-        
-        let attributes = contextLabel.attributesFromProperties()
-        
-        let foregroundColor = attributes[NSForegroundColorAttributeName] as UIColor
-        XCTAssertEqual(foregroundColor, UIColor.blueColor(), "")
-        
-        let shadow = attributes[NSShadowAttributeName] as NSShadow
-        let shadowColor = shadow.shadowColor as UIColor
-        XCTAssertEqual(shadowColor, UIColor.redColor(), "")
-        XCTAssertEqual(shadow.shadowOffset.width, CGFloat(1), "")
-        XCTAssertEqual(shadow.shadowOffset.height, CGFloat(2), "")
-        
-        let paragraphStyle = attributes[NSParagraphStyleAttributeName] as NSParagraphStyle
-        let paragraphStyleAlignment = paragraphStyle.alignment
-        XCTAssertEqual(paragraphStyleAlignment, NSTextAlignment.Center, "")
-        
-        // Highlighted
-        contextLabel.highlightedTextColor = UIColor.yellowColor()
-        contextLabel.highlighted = true
-        
-        let attributesHighlighted = contextLabel.attributesFromProperties()
-        
-        let foregroundColorHighlighted = attributesHighlighted[NSForegroundColorAttributeName] as UIColor
-        XCTAssertEqual(foregroundColorHighlighted, UIColor.yellowColor(), "")
-
-        // Disabled
-        contextLabel.enabled = false
-        
-        let attributesDisabled = contextLabel.attributesFromProperties()
-        
-        let foregroundColorDisabled = attributesDisabled[NSForegroundColorAttributeName] as UIColor
-        XCTAssertEqual(foregroundColorDisabled, UIColor.lightGrayColor(), "")
-    }
-    
-    func testGetRangesForLinksInAttributedString() {
-        
-        
-        
-    }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
 }
