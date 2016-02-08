0059232d2ac696b8ffdfe110a0d9cb8f9b0645a9
Michael Loistl <michael.loistl@gmail.com>
2016-02-08 17:00:55 +0000

    Add String extension to convert rangeFromNSRange and  NSRangeFromRange

--- ContextLabel/ContextLabel.swift
+++ ContextLabel/ContextLabel.swift
@@ -671,3 +671,23 @@ public class ContextLabel: UILabel, NSLayoutManagerDelegate {
         return textColor.colorWithAlphaComponent(0.5)
     }
 }
+
+extension String {
+    
+    func rangeFromNSRange(nsRange : NSRange) -> Range<String.Index>? {
+        let from16 = utf16.startIndex.advancedBy(nsRange.location, limit: utf16.endIndex)
+        let to16 = from16.advancedBy(nsRange.length, limit: utf16.endIndex)
+        if let from = String.Index(from16, within: self),
+            let to = String.Index(to16, within: self) {
+            return from ..< to
+        }
+        return nil
+    }
+    
+    func NSRangeFromRange(range : Range<String.Index>) -> NSRange {
+        let utf16view = self.utf16
+        let from = String.UTF16View.Index(range.startIndex, within: utf16view)
+        let to = String.UTF16View.Index(range.endIndex, within: utf16view)
+        return NSMakeRange(utf16view.startIndex.distanceTo(from), from.distanceTo(to))
+    }
+}
