a2d1da39bf519c8ac3186f2b32425e35ef75e77f
Michael Loistl <michael@aplo.io>
2017-03-27 16:23:11 +0100

    Add example project

--- ContextLabel/AppDelegate.swift
+++ ContextLabel/AppDelegate.swift
@@ -1,6 +1,6 @@
 //
 //  AppDelegate.swift
-//  Example
+//  ContextLabel
 //
 //  Created by Michael Loistl on 17/03/2017.
 //  Copyright © 2017 Aplo. All rights reserved.
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window!.rootViewController = UINavigationController(rootViewController: ViewController())
+        window!.makeKeyAndVisible()
+        
         return true
     }
 
--- ContextLabel/TableViewCell.swift
+++ ContextLabel/TableViewCell.swift
@@ -7,3 +7,89 @@
 //
 
 import Foundation
+import UIKit
+
+protocol TableViewCellDelegate: class {
+    func tableViewCell(_ sender: TableViewCell, didTouchWith touchResult: TouchResult)
+}
+
+class TableViewCell: UITableViewCell {
+    
+    weak var delegate: TableViewCellDelegate?
+    
+    lazy var contextLabel: ContextLabel = {
+        var frame = CGRect(x: 16, y: 0, width: self.contentView.bounds.width - 32, height: self.contentView.bounds.height)
+        
+        // Initialize ContextLabel
+        let _label = ContextLabel(frame: frame, didTouch: { (touchResult) in
+            self.delegate?.tableViewCell(self, didTouchWith: touchResult)
+        })
+        
+        // Custom Text Color (optional)
+        _label.foregroundColor = { (linkResult) in
+            switch linkResult.detectionType {
+            case .userHandle:
+                return .red
+            case .hashtag:
+                return .blue
+            case .url:
+                return .gray
+            case .textLink:
+                return .black
+            default:
+                return .black
+            }
+        }
+
+        // Custoim Underline Style (optional)
+        _label.underlineStyle = { (linkResult) in
+            switch linkResult.detectionType {
+            case .userHandle, .hashtag:
+                return .styleDouble
+            case .url:
+                return .styleSingle
+            case .textLink:
+                return .styleSingle
+            default:
+                return .styleNone
+            }
+        }
+        
+        // Any default UILabel properties
+        _label.font = .systemFont(ofSize: 16)
+        
+        return _label
+    }()
+    
+    // MARK: - Initializers
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        contentView.addSubview(contextLabel)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Super
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        switch contextLabel.touchState {
+        case .began:
+            break
+        default:
+            super.touchesBegan(touches, with: event)
+        }
+    }
+    
+    // MARK: - Methods
+    
+    func config(with text: String, textLinks: [TextLink]?) {
+        contextLabel.text = text
+        contextLabel.textLinks = textLinks
+    }
+    
+    
+}
